d2ab7c04ffc95c1d35043d090bd018c6706ded67
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-05 17:31:37 +0900

    Update documents

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ MongoClient.connect(
 	function(err, connection) {
 		var collection = connection.collection('customers');
 
-		collection.insert({'name': 'Jane Doe'}, function(err, count) {
+		collection.update(
+			{},
+			{'$set': {'age': 24}},
+			{'multi' : true},
+			function(err, count) {
+				console.log('Updated ', count, ' documents');
+				collection.find().toArray(function(err, documents) {
+					console.dir(documents);
+					connection.close();
+				});
+			}
+		);
+
+		/*collection.insert({'name': 'Jane Doe'}, function(err, count) {
 			collection.find().toArray(function(err, documents) {
 				console.dir(documents);
 				connection.close();
 			});
-		});
+		});*/
 	}
 );
